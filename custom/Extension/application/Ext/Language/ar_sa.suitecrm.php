9d94099ffb2c6dcae9fd0af184382ef870812a3d
abdennour <abdennour.toumi@gmail.com>
2014-10-21 14:33:22 +0300

    تعريب

---
+++ custom/Extension/application/Ext/Language/ar_sa.suitecrm.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package SuiteCRM
+ * @copyright SalesAgility Ltd http://www.salesagility.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * along with this program; if not, see http://www.gnu.org/licenses
+ * or write to the Free Software Foundation,Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * @author Salesagility Ltd <info@salesagility.com>
+ */
+
+$app_strings['LBL_QUICK_ACCOUNT'] = 'شركة عميل جديد';
+$app_strings['LBL_QUICK_CONTACT'] = 'إنشاء جهة اتصال';
+$app_strings['LBL_QUICK_OPPORTUNITY'] = 'إنشاء فرص عمل';
+$app_strings['LBL_QUICK_LEAD'] = 'إنشاء عملاء محتملين';
+$app_strings['LBL_QUICK_DOCUMENT'] = 'إنشاء وثيقة';
+$app_strings['LBL_QUICK_CALL'] = 'التسجيلات الهاتفية';
+$app_strings['LBL_QUICK_TASK'] = 'إنشاء مهمة';
+$app_strings['LBL_COLLECTION_TYPE'] = 'نوع';
+
+$app_strings['LBL_ADD_TAB'] = 'إضافة علامة تبويب';
+$app_strings['LBL_SUITE_DASHBOARD'] = 'Suiteالتحكم الرئيسة';
+$app_strings['LBL_ENTER_DASHBOARD_NAME'] = 'ادخل اسم لوحة التحكم الرئيسة:';
+$app_strings['LBL_NUMBER_OF_COLUMNS'] = 'عدد الأعمدة:';
+$app_strings['LBL_DELETE_DASHBOARD1'] = 'هل أنت متأكد من حذفها';
+$app_strings['LBL_DELETE_DASHBOARD2'] = 'هل ستحذفها?';
+$app_strings['LBL_ADD_DASHBOARD_PAGE'] = 'أضف صفحة في لوحة التحكم';
+$app_strings['LBL_DELETE_DASHBOARD_PAGE'] = 'إزالة هذه الصفحة من لوحة التحكم';
+$app_strings['LBL_RENAME_DASHBOARD_PAGE'] = 'إعادة تسمية الصفحة في لوحة التحك';
+
+$app_strings['LBL_DISCOVER_SUITECRM'] = 'Discover SuiteCRM';
+
+$app_list_strings['collection_temp_list'] = array ( 'Tasks' => 'المهام', 'Meetings' => 'الإجتماعات', 'Calls' => 'المكالمات', 'Notes' => 'الملاحظات&المذكرات', 'Emails' => 'البريد الإلكتروني' );
